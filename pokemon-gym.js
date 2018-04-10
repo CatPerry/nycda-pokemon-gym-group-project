3a376468bd5c7472b6cf83a87e8ff46d123e1fe6
Chris Keddell <ckeddell@gmail.com>
2018-04-10 13:45:53 -0400

    Tried to edit NAV, tried to have everything outside of the carousel images be transparent.

--- pokemon-gym.js
+++ pokemon-gym.js
@@ -1,29 +1,29 @@
-
-let pokeOneInfo = document.getElementById("")
-let listHTML;
-
-
-
-
-    for (i = 0; i < cataclysm.all.length; i++) {
-        let div = document.createElement("div")
-        // name
-        let listHTML = document.createElement("ul")
-        listHTML.innerHTML =` <li> ${cataclysm.all[i].name} </li>`;
-        console.log(cataclysm.all[i].name)
-        // hp name and hpstat
-            ` <li> ${cataclysm.all[i].hp} </li>`;
-        // //defense name and defense stat
-        // //attack name and attack stat
-        // //abilities name as a string
-        console.log(listHTML)
-
-        div.appendChild(listHTML)
-        section.appendChild(div)
-        document.body.appendChild(section)
-    }
-
-})
+//
+// let pokeOneInfo = document.getElementById("")
+// let listHTML;
+//
+//
+//
+//
+//     for (i = 0; i < cataclysm.all.length; i++) {
+//         let div = document.createElement("div")
+//         // name
+//         let listHTML = document.createElement("ul")
+//         listHTML.innerHTML =` <li> ${cataclysm.all[i].name} </li>`;
+//         console.log(cataclysm.all[i].name)
+//         // hp name and hpstat
+//             ` <li> ${cataclysm.all[i].hp} </li>`;
+//         // //defense name and defense stat
+//         // //attack name and attack stat
+//         // //abilities name as a string
+//         console.log(listHTML)
+//
+//         div.appendChild(listHTML)
+//         section.appendChild(div)
+//         document.body.appendChild(section)
+//     }
+//
+// })
 
 
 // Code below utilized from materialize for both the carousel and the the Floating
