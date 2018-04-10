f870b8a0adad4de029fca18ad4c44c08b84df34d
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-04-10 11:34:00 -0400

    Finally! Functionality for apending our trainer arrays to the DOM in our mashup file.

--- pokemon-gym.js
+++ pokemon-gym.js
@@ -3,32 +3,67 @@
 // pokeAll.push(catPokeArr)
 // console.log(pokeAll[0])
 
-// let pokePara = document.getElementsByClassName("all-pokemon")
+// let ul = document.createElement("ul")
 // let li = document.createElement("li")
+// let textNode = document.createTextNode("water")
+// li.appendChild(textNode)
+// document.getElementById("cat-pokemon").appendChild(li)
+
+
+let section = document.createElement("section")
+let listHTML;
+
 catPoke(function(cataclysm) {
-    console.log(cataclysm)
+    console.log(cataclysm.all)
+
+//     let li1 = document.createElement("li")
+//     let guzzlordPanel = document.getElementById("cat-pokemon")
+//     let guzzlord = cataclysm.get("guzzlord")
+//     li1.innerHTML = "<p>HP: " + cataclysm.hp + "</p> <p>Attack: " + cataclysm.attack +
+//         "</p> <p>Defense: " + cataclysm.defense + "</p>"
+
+//     li1.innerHTML += "<p>Abilities:"
+//     guzzlord.abilities.forEach(item => {
+
+//         li1.innerHTML += item + " / "
+//     })
+//     li1.innerHTML += "</p>"
+//     guzzlordPanel.appendChild(li1)  
+    
+// })
+    for (i = 0; i < cataclysm.all.length; i++) {
+        let div = document.createElement("div")
+        // name
+        let listHTML = document.createElement("ul")
+        listHTML.innerHTML =` <li> ${cataclysm.all[i].name} </li>`;
+        console.log(cataclysm.all[i].name)
+        // hp name and hpstat
+        // listHTML += '<li>' + cataclysm.all[i].name + '</li>';
+        // //defense name and defense stat
+        // listHTML += '<li>' + cataclysm.all[i].name + '</li>';
+        // //attack name and attack stat
+        // listHTML += '<li>' + cataclysm.all[i].name + '</li>';
+        // //abilities name as a string
+        // listHTML += '<li>' + cataclysm.all[i].name + '</li>';
+        // listHTML += '</ul>';
+        console.log(listHTML)
+
+        div.appendChild(listHTML)
+        section.appendChild(div)
+        document.body.appendChild(section)
+    }
+
 })
-console.log(cataclysm)
-// function printPokes(cataclysm) {
-//     let listHTML = '<ol>'
-//     for (i = 0; i < cataclysm.length; i++) {
-//         //name
-//         listHTML += '<li>' + cataclysm[0].name + '</li>';
-//         //hp name and hpstat
-//         listHTML += '<li>' + pokeAll[0] + '</li>';
-//         //defense name and defense stat
-//         listHTML += '<li>' + pokeAll[0] + '</li>';
-//         //attack name and attack stat
-//         listHTML += '<li>' + pokeAll[0] + '</li>';
-//         //abilities name as a string
-//         listHTML += '<li>' + pokeAll[0] + '</li>';
-//     }
-//     listHTML += '</ol>';
-//     return (listHTML);
-// }
+// printPokes(cataclysm)
+// console.log(cataclysm)
+// li.inner = "<p>DO this!!!</p>"
+// li.innerHTML = catPoke(function (cataclysm) {
+//     return "<p>DO this!!!</p>"
+// })
+// ul.appendChild(li)
+// document.body.appendChild(ul)
+
 
 // document.getElementsByClassName("all-pokemon").innerHTML = printPokes(pokeAll[0])
 // li.innerHTML = printPokes(pokeAll)
 // li.innerHTML = printPokes(pokeAll[0])
-// ul.appendChild(li)
-// document.body.appendChild(ul)
